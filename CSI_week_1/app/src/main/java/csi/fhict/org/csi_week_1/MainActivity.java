dc733d08bc4300daf35a6dabab33070c5d1d0410
subzerodeluxe <maarten.waanders@gmail.com>
2016-09-11 16:08:37 +0200

    Finished version

--- CSI_week_1/app/src/main/java/csi/fhict/org/csi_week_1/MainActivity.java
+++ CSI_week_1/app/src/main/java/csi/fhict/org/csi_week_1/MainActivity.java
@@ -1,7 +1,10 @@
 package csi.fhict.org.csi_week_1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSend = (Button) findViewById(R.id.bttnReport);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ CSI_week_1/app/src/main/java/csi/fhict/org/csi_week_1/ReportActivity.java
@@ -0,0 +1,34 @@
+package csi.fhict.org.csi_week_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ReportActivity extends AppCompatActivity {
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
